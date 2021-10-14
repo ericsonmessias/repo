bc45660cfabc5a96997ed0077f2ea0ed075605fe
ericsonmessias <erickson_messias.2011@hotmail.com>
2021-10-14 10:03:28 -0300

    commit]

--- main.tf
+++ main.tf
@@ -12,20 +12,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-    
-resource "aws_s3_bucket" "this"{
-    bucket = "my-terraform-s3-bcuket-20211008"
-    acl = "private"
-
-    tags = {
-        Name = "mybucket"
-        Created = "20211008"
-        Managedby = "Terraform"
-    }
-}
-
-resource "aws_s3_bucket_object" "this"{
-    bucket = aws_s3_bucket.this.bucket
-    key = "my-new-object"
-    source ="files/helloworld.txt"
-}
