e00cbdcb1b027249b42c1cf19a3b568f11d63340
ericsonmessias <erickson_messias.2011@hotmail.com>
2021-10-14 11:38:10 -0300

    commit

--- ec2.tf
+++ ec2.tf
@@ -7,9 +7,13 @@ data "aws_ami" "ubuntu"{
 resource "aws_instance" "web" {
   instance_type = "t4g.micro"
   ami           = data.aws_ami.ubuntu.id
-  user_data     = aws_s3_bucket_object.this.body
+  user_data     = aws_s3_bucket_object.this.content
 }
 
 output "aws_instance_ip" {
     value = aws_instance.web.public_ip
+}
+
+output "aws_s3_bucket_object" {
+  value = aws_s3_bucket_object.this.content
 }
